5066c9ee2be6caab46e923ddcba11d9af2cee99f
vikranth0212 <150246583+vikranth0212@users.noreply.github.com>
2024-01-24 23:41:36 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 # Create your views here.
 from app.forms import *
 from django.core.mail import send_mail
+from django.urls import reverse
+from django.contrib.auth import authenticate,login
+
 
 def registration(request):
     ufo=UserForm()
     pfo=ProfileForm()
     d={'ufo1':ufo,'pfo1':pfo}
+
+    #checking post method is activated or not
 
     if request.method=="POST" and request.FILES:
         ufd=UserForm(request.POST)
@@ -15,6 +20,10 @@ def registration(request):
         pfd=ProfileForm(request.POST,request.FILES)
         #for data and files or for only files we need to use
         #request.POST and request.FILES is mandatory
+
+        #to convert non modified function data objects to modified 
+        #we need to give commit=False
+
         if ufd.is_valid() and pfd.is_valid():
             MUFDO=ufd.save(commit=False)
             #for default it is true and it will give non modified data
@@ -30,21 +39,22 @@ def registration(request):
             MPFDO.username=MUFDO
             #here we are getting username column from another table i.e User table
             MPFDO.save()
-
+            
+            #sending the registration mail to user
             send_mail(
                 'registration',
                 'registration successfull',
-                'vikranth15.6.1995@gmail.com',
+                'vikranthfullstack@gmail.com',
                 [MUFDO.email],
                 fail_silently=False
             )
-            #send_mail('subject of mail in str','msg in str'
-            #'from mail id in str i.e it is application or company mail id'
-            #'user mail id who is signup from html page we need to give in list'
-            #it is coming in user object format in string
-            #if we are giving manually we need to give in str format
-            #fail_silently=True it will handle error and it will not show error
-            #if we five False it will not handle and throw error
+            # send_mail(1.'subject of mail in str',2.'msg in str'
+            # 3.'from mail id in str i.e it is application or company mail id'
+            #4. 'user mail id who is signup from html page we need to give in list'
+            # it is coming in user object format in string
+            # if we are giving manually we need to give in str format
+            # 5.fail_silently=True it will handle error and it will not show error
+            # if we five False it will not handle and throw error
 
 
 
@@ -53,3 +63,34 @@ def registration(request):
             return HttpResponse('invalid')
             
     return render(request,'registration.html',d)
+
+
+#Home page
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username} 
+        return render(request,'home.html',d)
+    return render(request,'home.html')       
+
+
+#login
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un'] 
+        password=request.POST['pw']  
+
+        AUO=authenticate(username=username,password=password) 
+
+        if AUO and AUO.is_active:
+            login(request,AUO)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('invalid credentials')
+    return render(request,'user_login.html') 
+
+# @login_required
+# def profile_display(request):
+#     un=request.session.get('username')
+#     UO=User.objects.get()
--- registration/urls.py
+++ registration/urls.py
@@ -19,7 +19,11 @@ from django.urls import path
 from app.views import *
 from django.conf.urls.static import static
 from django.conf import settings
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration/',registration,name='registration'),
+    path('user_login/',user_login,name='user_login'),
+    path('home/',home,name='home'),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
